5ccd2cc0cc9fc96cdf1722fe826f4adf417addf5
Shreyas Patil <shreyassp002@gmail.com>
2023-10-24 23:47:14 +0530

    Initialised Permissions for camera (cameraResultLauncher)

--- app/src/main/java/com/rey/permissionsdemo/MainActivity.kt
+++ app/src/main/java/com/rey/permissionsdemo/MainActivity.kt
@@ -1,16 +1,40 @@
 package com.rey.permissionsdemo
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import android.Manifest
+
 
 
 class MainActivity : AppCompatActivity() {
 
     private val cameraResultLauncher: ActivityResultLauncher<String> =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()){
+            isGranted ->
+            if (isGranted){
+                Toast.makeText(this, "Permission Granted For Camera", Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(this, "Permission Denied For Camera", Toast.LENGTH_LONG).show()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnCameraPermission: Button = findViewById(R.id.btnCameraPermission)
+        btnCameraPermission.setOnClickListener{
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)){
+
+            }
+        }
+
     }
 }
